d140143fca1edb32ffa0aa9c4d51c8412c0255d0
nayny009 <nayny009@mymail.unisa.edu.au>
2025-10-30 12:53:08 +1030

    Modularised "StrudelDemo" from "App"
    Moved everything out of App and into StrudelDemo instead.

---
+++ src/components/StrudelDemo.jsx
@@ -0,0 +1,107 @@
+import '../App.css';
+import { useEffect, useRef, useState } from "react";
+import { StrudelMirror } from '@strudel/codemirror';
+import { evalScope } from '@strudel/core';
+import { drawPianoroll } from '@strudel/draw';
+import { initAudioOnFirstClick } from '@strudel/webaudio';
+import { transpiler } from '@strudel/transpiler';
+import { getAudioContext, webaudioOutput, registerSynthSounds } from '@strudel/webaudio';
+import { registerSoundfonts } from '@strudel/soundfonts';
+import { stranger_tune } from '../tunes';
+import console_monkey_patch, { getD3Data } from '../console-monkey-patch';
+import ProcessInput from "./ProcessInput";
+import ProcessOutput from "./ProcessOutput";
+import ProcessButtons from "./ProcessButtons";
+import PlaybackButtons from "./PlaybackButtons";
+import DJControls from "./DJControls";
+import PageTitle from "./PageTitle";
+
+let globalEditor = null;
+
+const handleD3Data = (event) => {
+    console.log(event.detail);
+};
+
+export default function StrudelDemo() {
+
+    const hasRun = useRef(false);
+
+    const handlePlay = () => {
+        globalEditor.evaluate()
+    }
+
+    const handleStop = () => {
+        globalEditor.stop()
+    }
+
+    const [songText, setSongText] = useState(stranger_tune)
+
+    useEffect(() => {
+
+        if (!hasRun.current) {
+            document.addEventListener("d3Data", handleD3Data);
+            console_monkey_patch();
+            hasRun.current = true;
+            //Code copied from example: https://codeberg.org/uzu/strudel/src/branch/main/examples/codemirror-repl
+            //init canvas
+            const canvas = document.getElementById('roll');
+            canvas.width = canvas.width * 2;
+            canvas.height = canvas.height * 2;
+            const drawContext = canvas.getContext('2d');
+            const drawTime = [-2, 2]; // time window of drawn haps
+            globalEditor = new StrudelMirror({
+                defaultOutput: webaudioOutput,
+                getTime: () => getAudioContext().currentTime,
+                transpiler,
+                root: document.getElementById('editor'),
+                drawTime,
+                onDraw: (haps, time) => drawPianoroll({ haps, time, ctx: drawContext, drawTime, fold: 0 }),
+                prebake: async () => {
+                    initAudioOnFirstClick(); // needed to make the browser happy (don't await this here..)
+                    const loadModules = evalScope(
+                        import('@strudel/core'),
+                        import('@strudel/draw'),
+                        import('@strudel/mini'),
+                        import('@strudel/tonal'),
+                        import('@strudel/webaudio'),
+                    );
+                    await Promise.all([loadModules, registerSynthSounds(), registerSoundfonts()]);
+                },
+            });
+            document.getElementById('proc').value = stranger_tune
+        }
+        globalEditor.setCode(songText);
+    }, [songText]);
+
+
+    return (
+        <div>
+            <PageTitle />
+            <main>
+                <div className="container-fluid">
+                    <div className="row">
+                        <div className="col-md-8" style={{ maxHeight: '50vh', overflowY: 'auto' }}>
+                            <ProcessInput defaultValue={songText} onChange={(e) => setSongText(e.target.value)} />
+                        </div>
+                        <div className="col-md-4">
+                            <nav>
+                                <ProcessButtons />
+                                <br />
+                                <PlaybackButtons onPlay={handlePlay} onStop={handleStop} />
+                            </nav>
+                        </div>
+                    </div>
+                    <div className="row">
+                        <div className="col-md-8" style={{ maxHeight: '50vh', overflowY: 'auto' }}>
+                            <ProcessOutput />
+                        </div>
+                        <div className="col-md-4">
+                            <DJControls />
+                        </div>
+                    </div>
+                </div>
+                <canvas id="roll"></canvas>
+            </main >
+        </div >
+    );
+}
