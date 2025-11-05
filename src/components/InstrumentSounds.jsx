40469bba5e8f8b9dbbcd328a48c5b42ff0d78578
nayny009 <nayny009@mymail.unisa.edu.au>
2025-11-05 18:09:38 +1030

    Implemented buttons for drum sounds
    Created placeholder buttons for each of the drum sounds. Utilised simple bootstrap to organise the buttons and reused the button design.

---
+++ src/components/InstrumentSounds.jsx
@@ -0,0 +1,48 @@
+function InstrumentSounds() {
+  return (
+      <>
+          <div className="instruments">
+              <div className="row mb-3 g-3">
+                  <div className="col-2">
+                      <button id="BD" className="btn btn-green">BD</button>
+                  </div>
+                  <div className="col-2">
+                      <button id="BD" className="btn btn-green">SD</button>
+                  </div>
+                  <div className="col-2">
+                      <button id="BD" className="btn btn-green">RIM</button>
+                  </div>
+                  <div className="col-2">
+                      <button id="BD" className="btn btn-green">CP</button>
+                  </div>
+                  <div className="col-2">
+                      <button id="BD" className="btn btn-green">HH</button>
+                  </div>
+                  <div className="col-2">
+                      <button id="BD" className="btn btn-green">OH</button>
+                  </div>
+                  <div className="col-2">
+                      <button id="BD" className="btn btn-green">CR</button>
+                  </div>
+                  <div className="col-2">
+                      <button id="BD" className="btn btn-green">RD</button>
+                  </div>
+                  <div className="col-2">
+                      <button id="BD" className="btn btn-green">HT</button>
+                  </div>
+                  <div className="col-2">
+                      <button id="BD" className="btn btn-green">MT</button>
+                  </div>
+                  <div className="col-2">
+                      <button id="BD" className="btn btn-green">LT</button>
+                  </div>
+                  <div className="col-2">
+                      <button id="BD" className="btn btn-green">CB</button>
+                  </div>
+              </div>
+          </div>
+      </>
+  );
+}
+
+export default InstrumentSounds;
