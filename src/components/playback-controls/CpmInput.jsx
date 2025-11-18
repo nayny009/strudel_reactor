03baf6cc100231a692bcd9c0116e35f33c9eea68
nayny009 <nayny009@mymail.unisa.edu.au>
2025-11-18 19:42:35 +1030

    Updated cpm input
    Provided additional checks to ensure the user does not exceed a range between 1-200.

--- src/components/playback-controls/CpmInput.jsx
+++ src/components/playback-controls/CpmInput.jsx
@@ -1,8 +1,25 @@
 function CpmInput({ cpm, setCpm }) {
+    // Ensures the user can only set a value between 1-200.
+    const handleChangeCpm = (e) => {
+        const value = e.target.value;
+
+        // Allows users to delete the placeholder input to "".
+        if (value === "") {
+            setCpm("");
+            return;
+        }
+
+        const valueCheck = Number(value);
+
+        if (valueCheck >= 1 && valueCheck <= 200) {
+            setCpm(value);
+        }
+    };
+
   return (
       <div className="input-group mb-3">
           <span className="input-group-text" id="cpm_label">Set CPM</span>
-          <input type="text" className="form-control" id="cpm_text_input" placeholder="30" value={cpm} onChange={(e) => setCpm(e.target.value)} />
+          <input type="number" min="1" max="200" className="form-control" id="cpm_text_input" placeholder="30" value={cpm} onChange={handleChangeCpm} />
       </div>
   );
 }
